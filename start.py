642fdea484eb72571cc0e8bfb24f090d7e06e5c7
Pedro Luis Nicoletti <63631239+demolayy@users.noreply.github.com>
2022-03-03 11:24:44 +0000

    Add files via upload

---
+++ start.py
@@ -0,0 +1,52 @@
+
+import time
+import os
+import random
+import string
+import requests
+import json
+from requests.structures import CaseInsensitiveDict
+
+
+def sleep():
+    time.sleep(3)
+
+
+proxy = {
+    'http': 'http://kalupavi-rotate:j57xm6y2nac0@p.webshare.io:80/',
+    'https': 'http://kalupavi-rotate:j57xm6y2nac0@p.webshare.io:80/',
+}
+
+url = "https://tibia-b.site/entry.php"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+chars = string.ascii_letters + string.digits + '!@#$%^*()'
+random.seed = (os.urandom(1024))
+
+names = json.loads(open('names.json').read())
+sobrenomes = json.loads(open('sobrenome.json').read())
+value = True
+while(value):
+    for name in names:
+        try:
+            name_extra = ''.join(random.choice(string.digits))
+            segundo = ''.join(random.choice(sobrenomes))
+            dominio = ['@yahoo.com', '@gmail.com', '@uol.com.br',
+                       '@ig.com.br', '@bol.com.br', '@outlook.com']
+
+            mail = name.lower() + name_extra + segundo.lower() + random.choice(dominio)
+            pessoa = name.capitalize() + ' ' + segundo.capitalize()
+            password = ''.join(random.choice(chars) for i in range(8))
+
+            data = "loginemail="+mail+"&loginpassword="+password + \
+                "&autenticador=&page=overview&now=1646237443201"
+            sleep()
+            print(data)
+            resp = requests.post(url, headers=headers,
+                                 data=data, proxies=proxy)
+            print(resp.status_code)
+        except:
+            pass
